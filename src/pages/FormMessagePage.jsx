42d6adfdeeae23613c2b231285f86b38dae256e7
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-22 16:43:10 +0100

    added coms

--- src/pages/FormMessagePage.jsx
+++ src/pages/FormMessagePage.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 function FormMessagePage () {
-
+    // Je déclare mon hook useState avec une variable message et une fonction setMessage, je met une chaîne de caractère vide qui sera mise à jour quand j'écrirais dans mon
+    // input
     const [message, setMessage ] = useState("")
 
     const handleMessageChange = (event) => {
+        // J'appel ma fonction setMessage et je capture l'élément avec sa valeur pour dire que value = message
         setMessage(event.target.value);
        
     }
@@ -13,8 +15,10 @@ function FormMessagePage () {
         <form>
             <label>
                 Message : 
+                {/* Je déclare le type d'eventListener */}
             <input onChange={handleMessageChange} type="text" name="message" />
             </label>
+            {/* Je termine par ma variable message qui est mise à jour à chaque caractère écris */}
             <p>Vous avez envoyé : {message} </p>
         </form>
     )
